3a1cbf872518629c0f3768b6193c4c2b20a37107
DavidLisk88 <davidperrylisk@gmail.com>
2025-06-26 21:51:16 -0500

    Final Commit

--- src/main/java/org/yearup/configurations/DatabaseConfig.java
+++ src/main/java/org/yearup/configurations/DatabaseConfig.java
@@ -26,7 +26,6 @@ public class DatabaseConfig
 
     @Bean
     public ShoppingCartDao shoppingCartDao(DataSource dataSource, ProductDao productDao) throws SQLException {
-        System.out.println("Creating ShoppingCartDao bean...");
         return new MySqlShoppingCartDao(dataSource, productDao);
 
     }
